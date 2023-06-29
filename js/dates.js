84f4bd65675f1e5d6b9794d1d3c4968747470e10
rodribenitez <rodrimat99@hotmail.com>
2023-06-29 15:04:30 -0300

    dates.js

--- js/dates.js
+++ js/dates.js
@@ -1,38 +1,30 @@
- const url = "https://preprodsoydelivery.uy/aindicadoresweb.aspx"
+const url = "https://preprodsoydelivery.uy/aindicadoresweb.aspx"
  
-//  const datos ={
-//     "PG":3259,
-//     "TR":3258,
-//     "NA":518,
-//     "FA":321
-//  }
-
- const settings = {
-    method : 'POST',
-    mode : 'cors',
-    headers : {
-        'Content-Type': 'application/json'
-    }
- }
+const settings = {
+   method : 'POST',
+   mode : 'cors',
+   headers : {
+       'Content-Type': 'application/json'
+   }
+}
 
 
- const datos = fetch(url,settings).then(
-    res => res.json()
- ). then(
-    response => {
-        console.log(response.data);
-    }
- )
-  
+fetch(url,settings).then(
+   res => res.json()
+). then(
+   response => {
+       console.log(response);
+     const datos = response;
+     const pg = document.getElementById("pg")
+     const tr = document.getElementById("tr")
+     const na = document.getElementById("na")
+     const fa = document.getElementById("fa")
 
-const pg = document.getElementById("pg")
-const tr = document.getElementById("tr")
-const na = document.getElementById("na")
-const fa = document.getElementById("fa")
+     pg.innerText = datos.PG;
+     tr.innerText = datos.TR;
+     na.innerText = datos.NA;
+     fa.innerText = datos.FA;
+   }
+)
+ 
 
-
-
-pg.innerText = datos.PG
-tr.innerText = datos.TR
-na.innerText = datos.NA
-fa.innerText = datos.FA
